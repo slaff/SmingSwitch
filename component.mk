116783f51aad05b86b662f2bf737632cd7271e46
Slavey Karadzhov <slav@attachix.com>
2021-03-11 13:24:29 +0100

    Fix to work with latest Sming 4.3.0-pre .

--- component.mk
+++ component.mk
@@ -1,6 +1,5 @@
 # User Settings (See also: https://templates.blakadder.com/sonoff_TH.html)
 
-
 # PINS - these should match Sonoff TH16 models up to 2020
 LED_PIN ?= 13
 RELAY_PIN ?= 12
@@ -19,45 +18,7 @@ MAX_PINS ?= 32
 
 # End of User Settings
 
-
-## Application Component configuration
-## Parameters configured here will override default and ENV values
-## Uncomment and change examples:
-
-## Add your source directories here separated by space
-# COMPONENT_SRCDIRS := app
-# COMPONENT_SRCFILES :=
-# COMPONENT_INCDIRS := include
-
-## If you require any Libraries list them here
-# ARDUINO_LIBRARIES :=
-
-## List the names of any additional Components required for this project
 COMPONENT_DEPENDS := DHTesp UPnP-Schema
-
-## Set paths for any GIT submodules your application uses
-# COMPONENT_SUBMODULES :=
-
-## Append any targets to be built as dependencies of the project, such as generation of additional binary files
-# CUSTOM_TARGETS += 
-
-## Additional object files to be included with the application library
-# EXTRA_OBJ :=
-
-## Additional libraries to be linked into the project
-# EXTRA_LIBS :=
-
-## Provide any additional compiler flags
-# COMPONENT_CFLAGS :=
-# COMPONENT_CXXFLAGS :=
-
-## Configure flash parameters (for ESP12-E and other new boards):
-SPI_MODE := dio
-
-## SPIFFS options
-DISABLE_SPIFFS := 1
-# SPIFF_FILES = files
-
 
 ifneq ($(SMING_ARCH),Host)
 ENABLE_SMART_CONFIG = 1
